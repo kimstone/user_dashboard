302aa3556e3a15260d90dd843921f7e1b9d94829
kimstone <kimstone.va@gmail.com>
2021-02-19 16:13:18 -0500

    login works

--- apps/access_control/urls.py
+++ apps/access_control/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_view, name="access_control/dashboard"),
+    path('dashboard/admin/', views.admin_dashboard_view, name="access_control/dashboard_admin"),
     path('register/', views.show_registration_form_view, name="access_control/register"),
     path('register-me/', views.process_new_user_view, name="access_control/process_new_user"),
+    path('authenticate/', views.authenticate_user_view, name="access_control/authenticate"),
+    path('signin/', views.show_login_form_view, name="access_control/login_form"),
+    path('users/', views.user_directory_view, name="access_control/user_directory"),
 ]
--- apps/access_control/views.py
+++ apps/access_control/views.py
@@ -6,12 +6,41 @@ from . models import User
 
 # Create your views here.
 
+def admin_dashboard_view(request):
+    context = {
+        'page_title': "Admin Dashboard",
+        'page_meta_description': "SEO for Admin Dashboard",
+    }
+    return render(request, 'access_control/dashboard-admin.html', context)
+
+
+
 def dashboard_view(request):
     context = {
         'page_title': "Test App Dashboard",
         'page_meta_description': "SEO for Test App Dashboard",
     }
     return render(request, 'access_control/dashboard.html', context)
+
+
+
+def authenticate_user_view(request):
+
+    if request.method == "GET":
+        return redirect('/')
+
+    if not User.objects.authenticate_credentials(request.POST['email_add'], request.POST['password']):
+        messages.error(request, 'You have entered an invalid email/password combo')
+        return redirect('access_control/login_form')
+    else:
+        registered_user = User.objects.get(email=request.POST['email_add'])
+        request.session['user_id'] = registered_user.id
+        request.session['success_msg'] = "You have successfully logged in!"
+
+        if registered_user.user_level != 9:
+            return redirect('access_control/user_directory')
+        else:
+            return redirect('access_control/dashboard-admin')
 
 
 
@@ -35,7 +64,17 @@ def process_new_user_view(request):
             'page_title': "User Registration Success",
             'page_meta_description': "SEO for User Registration Success",
         }
-        return render(request, 'access_control/register-success.html', context)
+        #return render(request, 'access_control/register-success.html', context)
+        return redirect('access_control/login_form')
+
+
+
+def show_login_form_view(request):
+    context = {
+        'page_title': "User Login Form",
+        'page_meta_description': "SEO for User Login Form",
+    }
+    return render(request, 'access_control/login.html', context)
 
 
 
@@ -45,3 +84,11 @@ def show_registration_form_view(request):
         'page_meta_description': "SEO for User Registration",
     }
     return render(request, 'access_control/register.html', context)
+
+
+def user_directory_view(request):
+    context = {
+        'page_title': "User Directory Dashboard",
+        'page_meta_description': "SEO for User Directory Dashboard",
+    }
+    return render(request, 'access_control/user-directory.html', context)
